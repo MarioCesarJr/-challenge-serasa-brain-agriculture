0e10259633c0f14bc0420a932816562869406f03
Mario Cesar de Souza Junior <mcsjuniorsouza@gmail.com>
2025-02-23 17:42:13 -0300

    chore: update producer e2e test

---
+++ src/infra/http/controllers/update-producer.controller.e2e-spec.ts
@@ -0,0 +1,79 @@
+import { AppModule } from '@/infra/app.module';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+
+describe('Update producer (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+
+  test('[PUT] /producers', async () => {
+    const producerCreated = await prisma.producer.create({
+      data: {
+        cpfCnpj: '12345678000199',
+        name: 'Produtor Exemplo',
+        properties: {
+          create: [
+            {
+              farmName: 'Fazenda do Exemplo',
+              city: 'Cidade Exemplo',
+              state: 'Estado Exemplo',
+              totalArea: 1000,
+              arableArea: 800,
+              vegetationArea: 200,
+              crops: {
+                create: [
+                  {
+                    cropName: 'Soja',
+                    harvestYear: 2025,
+                  },
+                  {
+                    cropName: 'Milho',
+                    harvestYear: 2025,
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+      include: {
+        properties: {
+          include: {
+            crops: true,
+          },
+        },
+      },
+    });
+
+    const response = await request(app.getHttpServer())
+      .put(`/producers/${producerCreated.id}`)
+      .send({
+        cpfCnpj: '12345678000158',
+        name: 'Produtor Exemplo atualizado',
+      });
+
+    expect(response.statusCode).toBe(200);
+
+    const producerOnDatabase = await prisma.producer.findUnique({
+      where: {
+        cpfCnpj: '12345678000158',
+      },
+    });
+
+    expect(producerOnDatabase).toBeTruthy();
+  });
+});
